cf41d985c11d800d6dce8434df790402ae9eccd7
Mahi Jain <116392193+25jmahi29@users.noreply.github.com>
2025-07-20 19:12:17 -0500

    Update main.py
    Added correct code

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-# This code is written in python
+# Console 
+print("Let's talk about the Setting, Characters, and Context for Moroccan and Colombian women!\n")
+
+print("Settings: Morocco is a country in Northern Africa and Colombia is in South America and both are developing countries. In the data ectraction process, data was taken between 1992-2003 for Morocco and 1990-2015 for Colombia.\n")
+
+print("Characters: large majority of Moroccan women work in the agricultural industry and Colombian women live in urban areas due to work. Sharing education about women' rights in Morocco can prompt better access for work and participation in politics.\n")
+
+print("Context: Colombia Total Fertility Rate (TFR): 1.7 & Morocco TFR: 2.2. More reproductive freedoms for Colombian women.\n")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# print Research Question 
+print("After the initial gathering of data, the formulated research question is: What caused more urban migration for unmarried women in Colombia and Morocco? What can be some causes?\n")
+
+#Findings of the Research Question 
+print("Here are the findings and what may cause unmarried Colombian and Moroccan women to move to urban areas. \n")
+
+print("In the 1990s, there was a spike in violence against women in rural areas in Colombia.\n")
+
+print("The service sector was rapidly increasing, therefore a lot of women moved to urban areas for employment and fleeing from violence.\n")
+
+print("In the 1990s, Morocco faced decreasing agriculutral productivity causing more women and families to move to urban areas for employment and education.") 
+
+# For VNC
 # The pandas library is used for data processing and to read data files
 import pandas as pd 
 #The matplotlib library is used to plot histograms and scatter plots
@@ -8,28 +31,36 @@ import GWCutilities as util
 
 # Read a comma separated values (CSV) files into a variable
 # as a pandas DataFrame
+
+# If you wish change which data set you are working with, do that here: 
 lwd=pd.read_csv("livwell135.csv")
 
-# Print out the number of rows and columns
-print(lwd.shape)
+# Add Step 1 code here:
+listOfcountries = lwd["country_name"].unique()
+#print(util.vformat_list(listOfcountries))
 
-#  basic colors:
-# 'blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'white'
+#selecting two countries for comparison 
+oneCountryBooleanList = lwd["country_name"] == "Morocco"
+secondCountryBooleanList = lwd["country_name"] == "Colombia"
+#print(oneCountryBooleanList)
 
-# create a scatter plot
-plt.scatter(lwd["year"],lwd["ED_litt_p"],color="red")
+#  extracting data
+oneCountryData = lwd.loc[oneCountryBooleanList]
+secondCountryData = lwd.loc[secondCountryBooleanList]
 
-# add a title to the plot
-plt.title("Percent of Women Who are Literate over Time")
+#print (oneCountryData)
+# x and y variables 
+plt.xlabel("Women living in urban areas (%)")
+plt.ylabel("Women never married (%)") 
 
-#Label the x-axis
-plt.xlabel("Year")
+# different colors for different countries 
+plt.scatter(x="DM_urban_p", y="DM_nvr_marr_p", data = oneCountryData, color = 'blue')
+plt.scatter(x="DM_urban_p", y="DM_nvr_marr_p", data = secondCountryData, color = 'red')
 
-# label the y-axis
-plt.ylabel("Women who are literate (%)")
+# limit statements 
+plt.ylim(10, 50)
+plt.xlim(45, 85)
 
-# set the range for the y-axis
-plt.ylim(0,14)
-
-# show the plot
+# loc at top right hand cornor and made a legend 
+plt.legend(('Morocco', 'Colombia'))
 plt.show()
